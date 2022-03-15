92f02b6c86dd6c75771b13da5993410325b859f0
Marco Bertonati <marko.bertonati@gmail.com>
2022-03-14 23:19:30 -0300

    S added on 'construction'

--- src/components/undercontruction.js
+++ src/components/undercontruction.js
@@ -5,7 +5,7 @@ export default function UnderContruction() {
     <div className="h-full">
       <div className="absolute inset-0 bg-[#16182C]">
         <div className=" h-full flex flex-col mx-auto justify-center items-center">
-          <p className="text-[#63b8ea] font-bold">UNDER CONTRUCTION 🛠️</p>
+          <p className="text-[#63b8ea] font-bold">UNDER CONSTRUCTION 🛠️</p>
           <p className="text-white flex justify-content items-center">
             <a href="https://www.figma.com/proto/EMymcRRF6McQIhvjEPJfY4/Portfolio-Web-Marco-Bertonati?node-id=1%3A195&scaling=scale-down&page-id=0%3A1">
               👉 Check Figma project!
