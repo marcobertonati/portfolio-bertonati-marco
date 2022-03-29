7b9f5bf8f33f0e9310719e856aaf5da23081cfbd
Marco Bertonati <marko.bertonati@gmail.com>
2022-03-29 16:50:33 -0300

    =Fav icon and some config of google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,14 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Marco Bertonati | Web Developer",
+    titleTemplate: "%s · The Real Hero",
+    description: "Web page about Marco Bertonati.",
+    url: "https://www.portfoliobertonatimarco.gatsbyjs.io/", // No trailing slash allowed!
+    image: "/src/images/fav-icon-bertonati.svg", // Path to the image placed in the 'static' folder, in the project's root directory.
+    twitterUsername: "@marco_bertonati",
+    siteUrl: `https://www.portfoliobertonatimarco.gatsbyjs.io/`,
   },
   plugins: [
-    `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
@@ -11,8 +16,9 @@ module.exports = {
         trackingId: "G-J3NRMLVP4K",
         head: true,
         anonymize: true,
-        respectDNT: true
+        respectDNT: true,
       },
     },
+    `gatsby-plugin-postcss`,
   ],
 };
